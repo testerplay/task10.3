b294587e051b9617a9ca62e5903f8befc4fa5f0f
kulag <kulag@DESKTOP-CH7J89A>
2024-05-18 01:12:26 +0300

    Добавьте файлы проекта.

---
+++ task10.3/task10.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+bool isOddThreeDigitNumber(int number) {
+    // Проверка, что число трехзначное и непарное
+    return (number >= 100 && number <= 999) && (number % 2 != 0);
+}
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int number;
+    cout << "Введите целое положительное число: ";
+    cin >> number;
+
+    if (isOddThreeDigitNumber(number)) {
+        cout << "Число является непарным тризначным." << endl;
+    }
+    else {
+        cout << "Число НЕ является непарным тризначным." << endl;
+    }
+
+    return 0;
+}
